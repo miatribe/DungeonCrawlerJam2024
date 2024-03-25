57d1d9cf71f8221a054bd17f0b1389d7f7b45f84
Michael Wheatley <miatribe@users.noreply.github.com>
2024-03-24 20:14:59 -0400

    simple wall/door

--- Scenes/lever.gd
+++ Scenes/lever.gd
@@ -1,8 +1,12 @@
 extends Node3D
 
+@export var door:Node3D
 @onready var lever_left: MeshInstance3D = $Lever_Left
 
+var open:bool = false
 
 func interact() -> void:
-	lever_left.rotation_degrees.y += 180
-	print("I was interacted")
+	if !open:
+		lever_left.rotation_degrees.y += 180
+		open = true
+		door.position.y = 3
