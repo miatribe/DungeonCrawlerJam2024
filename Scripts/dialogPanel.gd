ef0994f255dd51d5593ed883e779df9f4f81d583
Michael Wheatley <miatribe@users.noreply.github.com>
2024-03-28 16:01:16 -0400

    dialog close button now shows time left

--- Scripts/dialogPanel.gd
+++ Scripts/dialogPanel.gd
@@ -5,6 +5,7 @@ extends MarginContainer
 @onready var member_icon: TextureRect = $PanelContainer/MarginContainer/VBoxContainer/Content/MemberIcon
 @onready var message_label: Label = $PanelContainer/MarginContainer/VBoxContainer/Content/MessageLabel
 @onready var auto_close: Timer = $AutoClose
+@onready var dialog_close_button: Button = $PanelContainer/MarginContainer/VBoxContainer/TopBar/DialogCloseButton
 
 
 var dialog:Dialog:
@@ -15,6 +16,7 @@ var dialog:Dialog:
 		member_icon.texture = dialog.speaker.image
 		message_label.text = dialog.message
 		auto_close.wait_time = dialog.autoCloseTime
+		dialog_close_button.autoCloseTime = dialog.autoCloseTime
 		auto_close.start()
 		visible = true
 
---
+++ Scripts/dialog_close_button.gd
@@ -0,0 +1,15 @@
+extends Button
+
+var autoCloseTime:float:
+	set(time_in):
+		autoCloseTime = time_in
+		progress_bar.value = 0
+		timer.wait_time = autoCloseTime
+		timer.start()
+
+@onready var progress_bar: ProgressBar = $ProgressBar
+@onready var timer: Timer = $ProgressBar/Timer
+
+
+func _process(_delta: float) -> void:
+	progress_bar.value = (int)((timer.time_left / autoCloseTime) * 100)
