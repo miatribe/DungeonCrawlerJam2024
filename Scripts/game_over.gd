267436c58805375d8f2e2f427ef9522aff6739f8
Michael Wheatley <miatribe@users.noreply.github.com>
2024-03-30 22:41:04 -0400

    level two triggers all done

--- Scripts/game_over.gd
+++ Scripts/game_over.gd
@@ -10,6 +10,13 @@ func on_game_over() -> void:
 	visible = true
 
 
+func on_game_victory() -> void:
+	$VBoxContainer/Title.text = "Thank you for playing!"
+	$VBoxContainer/Message.text = "Michael, Preston, and Nathan safely made it home. No one would believe their other-worldly story."
+	get_tree().paused = true
+	visible = true
+
+
 func _on_retry_button_pressed() -> void:
 	get_tree().paused = false
 	var level1 = load("res://Scenes/Levels/level_one.tscn")
--- Scripts/level_two.gd
+++ Scripts/level_two.gd
@@ -9,12 +9,41 @@ extends Node
  
 func _on_tenticle_one_tenticle_death() -> void:
 	$Props/FirstWall.position.y = 3 
-	#dialog_container.dialog = dialogs[0]
+	if $Triggers/Trigger3:
+		$Triggers/Trigger3.queue_free()
+
 
 
 func _on_tenticle_one_3_tenticle_death() -> void:
-	$Props/FogVolume.visible = true
-
+	$Props/Portal.visible = true
+	$Triggers/VictoryTrigger.visible = true
+	dialog_container.dialog = dialogs[6]
 
 func _on_tenticle_one_2_tenticle_death() -> void:
+	if $Triggers/Trigger5:
+		$Triggers/Trigger5.queue_free()
 	$Props/SecondWall.position.y = 3 
+
+
+func _on_trigger_triggered() -> void:
+	dialog_container.dialog = dialogs[0]
+
+
+func _on_trigger_2_triggered() -> void:
+	dialog_container.dialog = dialogs[1]
+
+
+func _on_trigger_4_triggered() -> void:
+	dialog_container.dialog = dialogs[2]
+
+
+func _on_trigger_3_triggered() -> void:
+	dialog_container.dialog = dialogs[3]
+
+
+func _on_trigger_5_triggered() -> void:
+	dialog_container.dialog = dialogs[4]
+
+
+func _on_trigger_6_triggered() -> void:
+	dialog_container.dialog = dialogs[5]
